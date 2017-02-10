c14222bfcd1031f6823b374d4e78e4894491725a
Li Gen <376792342@qq.com>
2017-02-10 18:00:46 +0800

    home page ui

--- SwiftWeibo/SwiftWeibo/Classes/Discovery/DiscoveryTableViewController.swift
+++ SwiftWeibo/SwiftWeibo/Classes/Discovery/DiscoveryTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DiscoveryTableViewController: UITableViewController {
+class DiscoverTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- SwiftWeibo/SwiftWeibo/Classes/Main/MainViewController.swift
+++ SwiftWeibo/SwiftWeibo/Classes/Main/MainViewController.swift
@@ -14,22 +14,31 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tabBar.tintColor = UIColor.orange
+        //home page
+        addChildViewController(childController: HomeTableViewController(),title: "首页",imageName: "tabbar_home")
+        addChildViewController(childController: MessageTableViewController(),title: "消息",imageName: "tabbar_message_center")
+        addChildViewController(childController: DiscoverTableViewController(),title: "广场",imageName: "tabbar_discover")
+        addChildViewController(childController: ProfileTableViewController(),title: "我",imageName: "tabbar_profile")
+        
     }
     
+    private func addChildViewController(childController: UIViewController,title:String,imageName:String) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        //1.1 home icon
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        
+        //1.2 nav bar
+        childController.title = title
+        
+        let nav = UINavigationController()
+        nav.addChildViewController(childController)
+        
+        addChildViewController(nav)
+        
     }
-    */
 
 }
+        
